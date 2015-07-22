c344f4e9a277b47ab28d362480d81bd5c645249c
rossacheson <ross.acheson@gmail.com>
2015-07-22 17:53:22 -0400

    Add Empty Tests Class

---
+++ src/YWDSiteTests.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/*
+ * IS2955 Software Testing Summer 2015
+ * Selenium and JUnit tests of http://www.rdadesigns.net/yw-d/
+ * 
+ * @author Ross Acheson
+ * @author Malini Santra
+ *
+ */
+
+public class YWDSiteTests {
+
+
+}
